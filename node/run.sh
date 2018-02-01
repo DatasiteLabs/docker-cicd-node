76bc10530f59d4be15a08433737bf13eeec150d2
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-02-01 16:41:00 -0600

    DSO-1644: CIDI-89: fixing bug where ENTRYPOINT is used and jenkins blows up after start when running cat becomes 'node cat'

--- node/run.sh
+++ node/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-docker build -t mrllsvc/node-git-ubuntu:latest .
+#docker build --no-cache --squash --pull -t mrllsvc/node-git-ubuntu:3 mrllsvc/node-git-ubuntu:latest .
 
 docker run \
 		-d -it --rm -p 3000:3000 \
 		--name node-git-ubuntu \
 		mrllsvc/node-git-ubuntu:latest
-
+#
+#docker push mrllsvc/node-git-ubuntu:3
 #docker push mrllsvc/node-git-ubuntu:latest
