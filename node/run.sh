d5b15f881182578db86a890b6b2f9b434caf083d
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-04-12 17:22:39 -0500

    CICD-120: updating git config for npm bump to work. add volume not files

--- node/run.sh
+++ node/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-[[ $1 ]] || { echo "missing an argument. first argument must be jfrog email" >&2; exit 1; }
-[[ $2 ]] || { echo "missing an argument. first argument must be jfrog token for user with email: ${1}" >&2; exit 1; }
-jfrog_email="${1}"
-jfrog_password="${2}"
+#[[ $1 ]] || { echo "missing an argument. first argument must be jfrog email" >&2; exit 1; }
+#[[ $2 ]] || { echo "missing an argument. first argument must be jfrog token for user with email: ${1}" >&2; exit 1; }
+#jfrog_email="${1}"
+#jfrog_password="${2}"
 
-docker_version=5
+docker_version=6
 
-${curDir}/inject-npmrc.sh ${jfrog_email} ${jfrog_password}
+#${curDir}/inject-npmrc.sh ${jfrog_email} ${jfrog_password}
 
 docker build --no-cache --pull -t mrllsvc/node-git-ubuntu:"${docker_version}" -t mrllsvc/node-git-ubuntu:latest .
 docker push mrllsvc/node-git-ubuntu:"${docker_version}"
