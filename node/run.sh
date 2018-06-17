d74ca17890a7e979856da58ed4974e525480ecea
Kayla Altepeter <kayla@kaylaaltepeter.com>
2018-06-17 12:04:54 -0500

    cleaningup from copy

--- node/run.sh
+++ node/run.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#[[ $1 ]] || { echo "missing an argument. first argument must be jfrog email" >&2; exit 1; }
-#[[ $2 ]] || { echo "missing an argument. first argument must be jfrog token for user with email: ${1}" >&2; exit 1; }
-#jfrog_email="${1}"
-#jfrog_password="${2}"
-
 docker_version=7
-
-#${curDir}/inject-npmrc.sh ${jfrog_email} ${jfrog_password}
 
 docker build --no-cache --pull -t mrllsvc/node-git-ubuntu:"${docker_version}" -t mrllsvc/node-git-ubuntu:latest .
 docker push mrllsvc/node-git-ubuntu:"${docker_version}"
