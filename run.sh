94fcb97faa0268ce1f07a502a2343347c173803f
Kayla Altepeter <kayla.altepeter@datasite.com>
2021-10-21 09:58:45 -0500

    fix(scripts): fix script issue

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ docker run \
 --rm -p 3000:3000 \
 -t -d -u 1000:1000 -v "$(pwd)":/home/node/workspace -w /home/node/workspace \
 --name docker-cicd-node-chrome \
-datasite:"${docker_version}-chrome" cat
+datasite/docker-cicd-node:"${docker_version}-chrome" cat
