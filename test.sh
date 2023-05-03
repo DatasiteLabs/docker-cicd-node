973e80fef417d613290fd7afd1f3b6981d08059e
kaltepeter <kayla.altepeter@datasite.com>
2023-05-03 09:18:18 -0500

    feat(node:16.19): update node and dependencies

--- test.sh
+++ test.sh
@@ -5,9 +5,11 @@ set -o nounset
 [[ ${DEBUG:-} == true ]] && set -o xtrace
 readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-LOCAL_NAME=datasite/test-cicd-node
+DOCKER_REPO=0.0.0.0:5001
+LOCAL_NAME=${DOCKER_REPO}/datasite/test-cicd-node
 
 echo "Running tests against ${LOCAL_NAME}"
 docker run --rm -v "${__dir}/test:/home/node/test" -w /home/node/test --user node "${LOCAL_NAME}:latest-chrome" bash -c "ls -la; npm ci; npm run test"
+docker push ${LOCAL_NAME}:latest-chrome
 docker run -it --user node test-cicd-node-consumer:latest
 # docker run --rm -v "${__dir}/test:/home/node/test:rw" -w /home/node/test --user root "${LOCAL_NAME}:latest-chrome" bash -c "export CYPRESS_CACHE_FOLDER=/home/node/test/.cache; npm ci; npm run test" # testing git action issues
--- test/mongo/util/db-handler.js
+++ test/mongo/util/db-handler.js
@@ -1,38 +1,43 @@
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
+let __MONGOINSTANCE;
 /**
  * Connect to the in-memory database.
  */
 module.exports.connect = async () => {
-    const instance = await MongoMemoryServer.create();
-    const uri = await instance.getUri();
-    __MONGOINSTANCE = instance;
-    process.env.MONGO_URI = uri.slice(0, uri.lastIndexOf('/'));
+  const instance = await MongoMemoryServer.create();
+  const uri = await instance.getUri();
+  __MONGOINSTANCE = instance;
+  process.env.MONGO_URI = uri.slice(0, uri.lastIndexOf("/"));
 
+  try {
     await mongoose.connect(`${process.env.MONGO_URI}/testdb`, {});
-    // await mongoose.connection.db.dropDatabase();
-}
+  } catch (error) {
+    console.error(error);
+  }
+  // await mongoose.connection.db.dropDatabase();
+};
 
 /**
  * Drop database, close the connection and stop mongod.
  */
 module.exports.closeDatabase = async () => {
-    await mongoose.disconnect();
-
-    const instance = __MONGOINSTANCE;
-    await instance.stop();
-}
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    if (__MONGOINSTANCE) {
+      await __MONGOINSTANCE.stop();
+    }
+};
 
 /**
  * Remove all the data for all db collections.
  */
 module.exports.clearDatabase = async () => {
-    // const collections = mongoose.connection.collections;
+  const collections = mongoose.connection.collections;
 
-    // for (const key in collections) {
-    //     const collection = collections[key];
-    //     await collection.deleteMany();
-    // }
-    await mongoose.connection.db.dropDatabase();
-}
+  for (const key in collections) {
+      const collection = collections[key];
+      await collection.deleteMany();
+  }
+};
