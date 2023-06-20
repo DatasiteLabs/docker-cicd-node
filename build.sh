296061bf4096b6381535d56df198e998d0b274bd
kaltepeter <kayla.altepeter@datasite.com>
2023-06-20 10:08:04 -0500

    feat(node): cutting release of node 18

--- build.sh
+++ build.sh
@@ -12,5 +12,6 @@ echo "Building ${LOCAL_NAME}"
 
 # Google Chrome is not currently supported on ARM64
 docker buildx build --builder=container --platform=linux/amd64 --output=type=docker --pull -t "${LOCAL_NAME}:latest-chrome" "${__dir}/"chrome-headless
+docker push "${LOCAL_NAME}:latest-chrome"
 # docker pull ${LOCAL_NAME}:latest-chrome
 docker buildx build --builder=container --platform=linux/amd64 --output=type=docker -t test-cicd-node-consumer --build-arg=DOCKER_REPO=${DOCKER_REPO} "${__dir}/test"
